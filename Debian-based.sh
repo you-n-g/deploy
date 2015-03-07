5e64cce3c2d354506f85c8c5041fd8fd88552457
Young <afe.young@gmail.com>
2015-03-07 02:33:38 +0000

    init deploy script

---
+++ Debian-based.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get install -y git vim-nox ruby ruby-mkrf ruby-dev exuberant-ctags
+
+# dpkg-reconfigure locales  # 配置成 LANG=en_US.UTF-8 就可以了
---
+++ deploy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -x
+
+REPO_PATH=`dirname "$0"`
+REPO_PATH=`cd "$REPO_PATH"; pwd`
+
+cd $REPO_PATH
+
+
+if which apt-get; then
+	bash Debian-based.sh
+fi
+
+if which yum; then
+	bash RPM-based.sh
+fi
+
+# config git
+git config --global user.name Young Yang
+git config --global user.email afe.young@gmail.com
+
+
+# clone repos
+cd ~
+if [ ! -e code_tools_repo ]; then
+	git clone https://github.com/you-n-g/code_tools_repo
+fi
+
+
+# config bashrc
+if ! grep "export EDITOR" ~/.bashrc ; then
+	echo "export EDITOR=`which vim`" >> ~/.bashrc
+fi
+
+# config vim
+if [ ! -e ~/.vimrc ]; then
+	cp code_tools_repo/code_to_copy/backend/etc/vimrc ~/.vimrc
+fi
+
+if [ ! -e ~/.vim ]; then
+    git clone https://github.com/tpope/vim-pathogen ~/.vim
+fi
+
+cd ~/.vim
+
+if [ ! -e bundle ]; then
+    git clone https://github.com/you-n-g/bundle
+    cd bundle
+    git submodule init
+    git submodule update
+fi
+
+## config for command-t
+cd ~/.vim/bundle/command-t/ruby/command-t/
+ruby extconf.rb && make
