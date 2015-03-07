6d49b322b1c3f630d7fc979f13e483698e4eb271
you-n-g <afe.young@gmail.com>
2015-03-07 19:02:09 +0800

    Update Debian-based.sh
    add apt-get install screen

--- Debian-based.sh
+++ Debian-based.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get install -y git vim-nox ruby ruby-mkrf ruby-dev exuberant-ctags
+apt-get install -y git vim-nox ruby ruby-mkrf ruby-dev exuberant-ctags screen
 
 # dpkg-reconfigure locales  # 配置成 LANG=en_US.UTF-8 就可以了
