8cc94cf9324233d7026f01b00dfe6d49fc359da9
litterbear <liyilin1214@gmail.com>
2016-05-18 08:47:44 +0800

    add python pip

--- RPM-based.sh
+++ RPM-based.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum groupinstall 'Development Tools' 
 
-yum install -y git tmux cmake python-devel clang htop python-flake8
+yum install -y git tmux cmake python-devel clang htop python-flake8 python-pip
 # python-flake8 is needed by vim-flake8 
 # clang is for YCM
