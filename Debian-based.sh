ba54a1804f45ec98f48f0fd7d2d4acae3ccda9af
Young <afe.young@gmail.com>
2016-06-09 14:20:57 +0800

    supporting ubuntu 16.04

--- Debian-based.sh
+++ Debian-based.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y git vim-nox ruby ruby-mkrf ruby-dev exuberant-ctags tmux build-essential cmake python-dev \
-    clang htop python-flake8
+sudo apt-get install -y git vim vim-nox ruby ruby-dev exuberant-ctags tmux build-essential cmake python-dev \
+    clang htop python-flake8 # this line is for common use
+
+# these should installed separately. Otherwise it will fail together.
+sudo apt-get install -y ruby-mkrf # this line is for ubuntu 14.04
+sudo apt-get install -y vim-nox-py2  # this line is for ubuntu 16.04
+
 # clang is for YCM
-# python-flake8 is needed by vim-flake8 
+# python-flake8 is needed by vim-flake8
 
 # dpkg-reconfigure locales  # 配置成 LANG=en_US.UTF-8 就可以了
