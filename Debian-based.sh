6a4db5ec12a35ce94d1d4003bb1f68030f48d5d5
Young <afe.young@gmail.com>
2016-06-16 08:46:11 +0800

    normal commit

--- Debian-based.sh
+++ Debian-based.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt-get install -y git vim-nox ruby ruby-mkrf ruby-dev exuberant-ctags tmux build-essential cmake python-dev \
     clang htop python-flake8
 # clang is for YCM
-# python-flake8 is needed by vim-flake8 
+# python-flake8 is needed by vim-flake8
 
 # dpkg-reconfigure locales  # 配置成 LANG=en_US.UTF-8 就可以了
