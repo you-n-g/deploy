dff449636599da40ebf5a13b6b1098adc1fbbbcc
Young <afe.young@gmail.com>
2017-01-23 10:09:35 +0800

    start_tmux.sh

--- helper_scripts/start_tmux.sh
+++ helper_scripts/start_tmux.sh
@@ -6,5 +6,10 @@ cd ~/deployment4personaluse/
 tmux new-window -n deployment -t code_repo:1
 
 
+# make sure you copy the .ssh/config already
+# cp ~/code_tools_repo/code_to_copy/backend/etc/ssh_config/ ~/.ssh/config
+# tmux new-session -s proxy -n linode -d "sleep 5 && autossh -M 9001 linode -R0.0.0.0:6489:0.0.0.0:6489"
+# tmux new-window -n aliyun -t proxy:1 "sleep 5 && autossh -M 9002 aliyun -R0.0.0.0:6489:0.0.0.0:6488"
+
 # tmuxinator 是一个值得看一下的项目
 # https://github.com/tmuxinator/tmuxinator
