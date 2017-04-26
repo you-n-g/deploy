5fe5e1171db51731194c13b52ef93499d9dec4ed
Young <afe.young@gmail.com>
2017-04-26 17:27:36 +0800

    add proxy monitor

--- deploy_apps/deploy_proxy.sh
+++ deploy_apps/deploy_proxy.sh
@@ -13,3 +13,20 @@ EOF
 fi
 
 service polipo restart
+
+
+# add monitor to make polipo stable
+# read doc about monit here: http://www.tecmint.com/how-to-install-and-setup-monit-linux-process-and-services-monitoring-program/
+
+sudo apt-get install monit
+cat > /etc/monit/conf.d/polipo <<EOF
+ check process polipo with pidfile /var/run/polipo/polipo.pid
+   group proxy
+   group proxy
+   start program = "/usr/sbin/service polipo start"
+   stop program  = "/usr/sbin/service polipo stop"
+   if failed host 127.0.0.1 port 6489 type tcp then restart
+EOF
+
+sed -i 's/set daemon 120/set daemon 60/' /etc/monit/monitrc
+service monit restart
