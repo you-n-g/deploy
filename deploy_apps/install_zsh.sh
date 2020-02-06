69e4be866ad3ac582ca4a14174d8cc98cbabfbd7
Young <afe.young@gmail.com>
2020-02-06 01:39:13 +0000

    update zsh environment

--- deploy_apps/install_zsh.sh
+++ deploy_apps/install_zsh.sh
@@ -49,6 +49,35 @@ antigen apply
 EOF
 fi
 
+if ! grep "^# For showing time" $RC_FILE ; then
+	cat >> $RC_FILE << "EOF"
+# For showing time
+# show right prompt with date ONLY when command is executed
+preexec () {
+    DATE=$( date +"[%H:%M:%S]" )
+    local len_right=$( strlen "$DATE" )
+    len_right=$(( $len_right+1 ))
+    local right_start=$(($COLUMNS - $len_right))
+
+    local len_cmd=$( strlen "$@" )
+    local len_prompt=$(strlen "$PROMPT" )
+    local len_left=$(($len_cmd+$len_prompt))
+
+    RDATE="\033[${right_start}C ${DATE}"
+
+    if [ $len_left -lt $right_start ]; then
+        # command does not overwrite right prompt
+        # ok to move up one line
+        echo -e "\033[1A${RDATE}"
+    else
+        echo -e "${RDATE}"
+    fi
+}
+# https://stackoverflow.com/a/26585789
+export PROMPT="[%D{%H:%M:%S}] $PROMPT"
+EOF
+fi
+
 cd $REPO_PATH
 . ../helper_scripts/config_rc.sh
 
