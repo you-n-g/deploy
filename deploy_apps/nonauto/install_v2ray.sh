7c4bd44d0a548d3747fb847f400dd7f92968f857
Young <afe.young@gmail.com>
2020-08-12 15:09:43 +0000

    add v2ray and no auto scripts

---
+++ deploy_apps/nonauto/install_v2ray.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+APP_PATH=~/apps/v2ray
+
+mkdir -p $APP_PATH
+
+
+cd $APP_PATH
+
+wget https://github.com/v2ray/v2ray-core/releases/download/v4.27.0/v2ray-linux-64.zip
+
+unzip  v2ray-linux-64.zip
+
+UUID=`python -c 'import uuid; print(uuid.uuid4())'`
+
+
+cat > config.json  <<EOF
+{
+  "inbounds": [
+    {
+      "port": 5432, // 服务器监听端口
+      "protocol": "vmess",    // 主传入协议
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "alterId": 64
+          }
+        ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",  // 主传出协议
+      "settings": {}
+    }
+  ]
+}
+EOF
+
+cat > config_client.json <<EOF
+{
+  "inbounds": [
+    {
+      "port": 9997, // 监听端口
+      "protocol": "socks", // 入口协议为 SOCKS 5
+      "sniffing": {
+        "enabled": true,
+        "destOverride": ["http", "tls"]
+      },
+      "settings": {
+        "auth": "noauth"  //socks的认证设置，noauth 代表不认证，由于 socks 通常在客户端使用，所以这里不认证
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "vmess", // 出口协议
+      "settings": {
+        "vnext": [
+          {
+            "address": "<SERVER_ADDRSS>", // 服务器地址，请修改为你自己的服务器 IP 或域名
+            "port": 5432,  // 服务器端口
+            "users": [
+              {
+                "id": "$UUID",  // 用户 ID，必须与服务器端配置相同
+                "alterId": 64 // 此处的值也应当与服务器相同
+              }
+            ]
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+
+echo 'Install finished. Please run ./v2ray'
+
+# 如果 client想启动一个程序连多个 v2ray
+# 需要在每组 inbounds 和 outbounds 的最外层dict 都加上  "tag": "in-wall",
+# 并且在最后的 routing.rules 加上下面的内容
+# "routing": {
+#     "rules": [
+#         {
+#             "type": "field",
+#             "inboundTag": ["in-wall"],
+#             "outboundTag": "in-wall"
+#         },
+#     ]
+# }
