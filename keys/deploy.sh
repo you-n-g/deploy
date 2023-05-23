2bf4119765d8900f2e180a045b403f1fcd087db0
Young <afe.young@gmail.com>
2023-05-23 04:19:00 +0000

    Fix config path

--- keys/deploy.sh
+++ keys/deploy.sh
@@ -4,8 +4,9 @@ DIR="$( cd "$(dirname "$(readlink -f "$0")")" || exit ; pwd -P )"
 mkdir -p ~/.gnupg/
 unlink ~/.gnupg/gpg-agent.conf
 
-ln -s ~/deploy/configs/configs/misc/gpg-agent.conf ~/.gnupg/
+ln -s ~/deploy/configs/misc/gpg-agent.conf ~/.gnupg/
 
 # reload gpg
 gpgconf --kill gpg-agent
 gpg-agent --daemon
+# gpg -d keys/gpt.gpg
