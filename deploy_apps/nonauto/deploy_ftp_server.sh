784b6c4a6c12ddee5a3f8db9f14cb2b23fa0b64a
Young <afe.young@gmail.com>
2023-11-17 10:26:31 +0000

    Add parameter for ftp

--- deploy_apps/nonauto/deploy_ftp_server.sh
+++ deploy_apps/nonauto/deploy_ftp_server.sh
@@ -3,10 +3,39 @@
 # this is from
 # https://hub.docker.com/r/delfer/alpine-ftp-server 
 
+username="user"
+password="password"
+directory="`pwd`/files"
+
+while getopts ":u:p:d:" opt; do
+    case $opt in
+        u)
+        echo "-u was triggered, Parameter: $OPTARG" >&2
+        username=$OPTARG
+        ;;
+        p)
+        echo "-p was triggered, Parameter: $OPTARG" >&2
+        password=$OPTARG
+        ;;
+        d)
+        echo "-d was triggered, Parameter: $OPTARG" >&2
+        directory=$OPTARG
+        ;;
+        \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+        :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+    esac
+done
+
 sudo docker run -d \
     -p 21:21 \
     -p 21000-21010:21000-21010 \
-    -v `pwd`/files:/afiles/ \
-    -e USERS="<username>|<password>|/afiles/|`id -u`" \
+    -v $directory:/afiles/ \
+    -e USERS="$username|$password|/afiles/|`id -u`" \
     -e ADDRESS=0.0.0.0 \
     delfer/alpine-ftp-server
