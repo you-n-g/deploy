68dd6b7e4205a684974d43abda6e3f44e3f4ea1c
Young <afe.young@gmail.com>
2024-06-19 12:03:33 +0800

    Refine config

--- configs/lazynvim/lua/plugins/editing.lua
+++ configs/lazynvim/lua/plugins/editing.lua
@@ -112,9 +112,10 @@ return {
       -- vim.g.vimtex_view_general_options = vim.fn.getcwd() .. "/main.pdf"
       -- -- strip the left "/mnt/c/" part in vim.g.vimtex_view_general_options if exists
       -- vim.g.vimtex_view_general_options = vim.g.vimtex_view_general_options:gsub("^/mnt/c", "")  -- the prefix / must be kept.
-      vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"  -- this will be much simpler than above methods
-      -- TODO: add `-forward-search @tex @line` before @pdf
+      -- NOTE: it will not work when pwd does not match with the location of @tex.  It will concat the relative path with the @tex location.
+      -- vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"  -- this will be much simpler than above methods
       -- NOTE: the path does not work. It only works after adding deploy/helper_scripts/bin/cygpath into the bin search path
+      vim.g.vimtex_view_general_options = "-reuse-instance @pdf"  --It seems the path to @tex can't be rightly handled. So we skip them
 
       -- NOTE: set default to main.tex (We don't need this now)
       -- It seems that the vimtex will prompt you to make the choice. But it may affect `simplegpt.nvim` everytime when you create a buffer with specific type.
