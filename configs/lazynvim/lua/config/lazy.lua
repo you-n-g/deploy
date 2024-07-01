2a6919753f1f5b5648938d142413d3c6e8971ca0
Young <afe.young@gmail.com>
2024-07-01 05:01:16 +0000

    Fix luasnip after native neovim snippets

--- configs/lazynvim/lua/config/lazy.lua
+++ configs/lazynvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },  -- TODO: check if it really works.
+    { import = "lazyvim.plugins.extras.coding.luasnip" },  -- Disable Native snippets; Use my preferred `luasnip` that can load snippets from more sources
     { import = "plugins" },
   },
   defaults = {
