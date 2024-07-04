70ef0f712d3ad3be341723c91a9c987db4b68f2a
Young <afe.young@gmail.com>
2024-07-04 04:48:31 +0000

    Update package install

--- deploy_apps/install_fav_py_pack.sh
+++ deploy_apps/install_fav_py_pack.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Known issues of the script
-# - [ ] it may override `configs/jupyter/snippets.json`
+set -x # I still have a lot of issue about this script. So make it more verbose.
+# Known issues of
+# - the installation process:
+#   - You may need  `pip install -U --force-reinstall charset-normalizer` somewhere
+# - the script(issues even installed successfully)
+#   - [ ] it may override `configs/jupyter/snippets.json`
 
 # # Outlines: user-wise instead of environment-wise dependencies
 
@@ -11,8 +15,8 @@ export PATH="$PATH:$HOME/.local/bin"
 pip install --user neovim pynvim
 # `pipenv virtualenv` are not installed in this way due to it should bind with a specific python env
 for p in pre-commit ranger-fm yapf black copier tldr; do
-    # pipx will install things in user space
-    pipx install $p
+  # pipx will install things in user space
+  pipx install $p
 done
 # other favorite candidates
 # - pipx install asciinema
@@ -24,19 +28,18 @@ done
 # - pip install pipenv -U
 # - pip install virtualenv -U
 
-
 # # Outlines: environment-wise dependencies
 # TODO: extract following system-wide dependencies to the above section.
 
 # install some essential scripts
 # conda install -y tensorflow-gpu keras-gpu
-conda install -y pandas matplotlib ipywidgets scikit-learn seaborn ipyparallel # some software we should reinstall if we recreate a new environment
+conda install -y pandas matplotlib ipywidgets scikit-learn seaborn # some software we should reinstall if we recreate a new environment
 conda install -y -c conda-forge python-cufflinks shellcheck
 # sudo apt-get install -y libmysqlclient-dev  # https://stackoverflow.com/a/5178698
 # pip install mysqlclient  # for python3
 # pip install tensorboardX
 # conda install -y tensorboard
-pip install papermill ipdb nbresuse jupytext # pdbpp
+pip install ipdb nbresuse jupytext # pdbpp
 # - jupytext: 必须先安装再启动jupyter， 不然不会自动帮你保存py文件
 # - pdbpp: sticky 功能非常好用!!!!
 #   - NOTE: pdbpp is not compatible with jupyter debugging.
@@ -50,22 +53,15 @@ conda install -c conda-forge -y jupyter_contrib_nbextensions
 conda install -c conda-forge -y pyarrow
 # - this is for supporting parquet (data format decoupled with code and env)
 
+jupyter contrib nbextension install
 
-# 这些都是针对个人用户的， 别人安装了没有用
-jupyter contrib nbextension install  # --user
-
-for plg in toc2/main select_keymap/main execute_time/ExecuteTime scratchpad/main notify/notify codefolding/main collapsible_headings/main snippets/main
-do
-    jupyter nbextension enable $plg
+for plg in toc2/main select_keymap/main execute_time/ExecuteTime scratchpad/main notify/notify codefolding/main collapsible_headings/main snippets/main; do
+  jupyter nbextension enable $plg
 done
 
-# conda create -y -n py27 python=2.7
-
-
-# for developing environment
+# ## Outlines: for development & configuration
 pip install autopep8 better_exceptions ipython-autotime fire pylint debugpy objexplore
 # ruff-lsp #  ruff-lsp only give warnings and does not provide docs. So it can't replace pyright.
-
 
 # candidates packages
 # - https://github.com/tartley/colorama
@@ -76,17 +72,14 @@ FILE_P=$DIR_P/snippets.json
 rm "$FILE_P"
 ln -s ~/deploy/configs/jupyter/snippets.json "$FILE_P"
 
-if [[ ! -e ~/.pdbrc.py  ]]; then
-    ln -s ~/deploy/configs/python/pdbrc.py ~/.pdbrc.py
+if [[ ! -e ~/.pdbrc.py ]]; then
+  ln -s ~/deploy/configs/python/pdbrc.py ~/.pdbrc.py
 fi
-
 
 cd ~/deploy/
 python deploy_apps/deploy_plot_cn_font.py
 
 sh deploy_apps/set_jupyter_pwd.sh
-
-
 
 # 一般常用的软件
 pip install nose ipdbplugin
@@ -95,13 +88,13 @@ pip install nose ipdbplugin
 pip install jupyter_ascending
 
 jupyter nbextension install --py --sys-prefix jupyter_ascending
-jupyter nbextension     enable jupyter_ascending --sys-prefix --py
+jupyter nbextension enable jupyter_ascending --sys-prefix --py
 jupyter serverextension enable jupyter_ascending --sys-prefix --py
 # below is for checking if the extension is installed
 # jupyter nbextension list
 # jupyter serverextension list
 # CONFIG & Usage: After installing the command above and vim plugin, you should also install notice following things:
-# - you must specify the port before starting **both** vim and jupyter, e.g. `JUPYTER_ASCENDING_EXECUTE_PORT=9000`; 
+# - you must specify the port before starting **both** vim and jupyter, e.g. `JUPYTER_ASCENDING_EXECUTE_PORT=9000`;
 #    - 这个端口要和jupyter的端口一致!!!
 # - Make sure both of them can be accessed by 127.0.0.1, otherwise you should also set the IP by `JUPYTER_ASCENDING_EXECUTE_HOST=xxxxxxx`
 # - Make sure the vim end can access without credentials ` --NotebookApp.token='' --NotebookApp.password=''`
@@ -114,3 +107,7 @@ jupyter serverextension enable jupyter_ascending --sys-prefix --py
 # - Base on my experience, you must restart the kernel after you install the plugin, otherwise it will not work
 # Others:
 # - autosave is controlled by magic function in jupyter like `%autosave 0`
+
+# ## Outlines: Deprecated packages:
+# - conda:  ipyparallel
+# - pip: papermill
