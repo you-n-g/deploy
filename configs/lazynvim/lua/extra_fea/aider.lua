e7f2a569d39b15db7b2fa3ea84f4d5bbdf5cb012
Young <afe.young@gmail.com>
2024-12-07 15:40:53 +0000

    (no commit message provided)

--- configs/lazynvim/lua/extra_fea/aider.lua
+++ configs/lazynvim/lua/extra_fea/aider.lua
@@ -1,9 +1,4 @@
 --[[
-map <local>ral to send two command to terminal
-key_shell.sh azure_aider
-aider --model $CHAT_MODEL  --no-auto-commit
-aider = AiderREPL
-
 Related projects
 - https://github.com/aweis89/aider.nvim
 ]]
@@ -13,30 +8,16 @@ local term_size = 12
 
 local repl_inst = repl.REPLFactory()
 
-local launch_cmd = [[key_shell.sh %s bash -c "aider --model \$CHAT_MODEL --no-auto-commit --no-show-model-warnings --editor \"nvim --cmd 'let g:flatten_wait=1' --cmd 'cnoremap wq lua vim.cmd(\\\"w\\\"); require\\\"snacks\\\".bufdelete()'\" --watch-files"]]
--- local launch_cmd = [[key_shell.sh %s bash -c "aider --model \$CHAT_MODEL --no-auto-commit --no-show-model-warnings --editor \"nvim --cmd 'let g:flatten_wait=1' --cmd 'cnoremap wq lua vim.cmd(\\\"w\\\"); require\\\"snacks\\\".bufdelete()'\" --watch-files"]]
--- local launch_cmd = [[key_shell.sh %s bash -c "aider --model \$CHAT_MODEL --no-auto-commit --no-show-model-warnings --editor \"nvim --cmd 'let g:flatten_wait=1'\" "]]
+local launch_cmd = [[key_shell.sh %s bash -c "aider --model \$CHAT_MODEL --weak-model \$CHAT_MODEL --no-auto-commit --no-show-model-warnings --editor \"nvim --cmd 'let g:flatten_wait=1' --cmd 'cnoremap wq lua vim.cmd(\\\"w\\\"); require\\\"snacks\\\".bufdelete()'\" --watch-files"]]
+-- local launch_cmd = [[key_shell.sh %s bash -c "aider --model \$CHAT_MODEL --weak-model \$CHAT_MODEL --no-auto-commit --no-show-model-warnings --editor \"nvim --cmd 'let g:flatten_wait=1' --cmd 'cnoremap wq lua vim.cmd(\\\"w\\\"); require\\\"snacks\\\".bufdelete()'\" --watch-files"]]
+-- local launch_cmd = [[key_shell.sh %s bash -c "aider --model \$CHAT_MODEL --weak-model \$CHAT_MODEL --no-auto-commit --no-show-model-warnings --editor \"nvim --cmd 'let g:flatten_wait=1'\" "]]
 
 vim.keymap.set("n", "<leader>raL", function()
-  -- local cmds = {
-  --   "key_shell.sh azure_aider",
-  --   "aider --model azure/$CHAT_MODEL --no-auto-commit",
-  -- }
-  -- for _, cmd in ipairs(cmds) do
-  --   require("toggleterm").exec(cmd, tonumber(vim.g.toggleterm_last_id), 12)
-  -- end
   require("toggleterm").exec(string.format(launch_cmd, "azure_aider"), tonumber(vim.g.toggleterm_last_id), term_size)
 end, { noremap = true, silent = true, desc = "Run azure_aider commands in terminal" })
 
 
 vim.keymap.set("n", "<leader>ral", function()
-  -- local cmds = {
-  --   "key_shell.sh azure_ad_aider",
-  --   "aider --model azure/$CHAT_MODEL --no-auto-commit --no-show-model-warnings",
-  -- }
-  -- for _, cmd in ipairs(cmds) do
-  --   require("toggleterm").exec(cmd, tonumber(vim.g.toggleterm_last_id), 12)
-  -- end
   require("toggleterm").exec(string.format(launch_cmd, "openai_lite"), tonumber(vim.g.toggleterm_last_id), term_size)
 end, { noremap = true, silent = true, desc = "Run azure_ad_aider commands in terminal" })
 
