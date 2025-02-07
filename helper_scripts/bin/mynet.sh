cd164e7295bac3211b6ae270642c28403a1d67b9
Young <afe.young@gmail.com>
2025-02-07 12:49:59 +0000

    feat: Add create_tunnel function to mynet.sh script

--- helper_scripts/bin/mynet.sh
+++ helper_scripts/bin/mynet.sh
@@ -9,6 +9,17 @@ install_cf() {
   chmod a+x cloudflared
 }
 
+create_tunnel() {
+ cloudflared tunnel login
+ cloudflared tunnel create rss  # create a tunnel
+ cloudflared tunnel list
+ # add ~/.cloudflared/config.yml  # map to tunnel to local address
+ cloudflared tunnel route ip show
+ cloudflared tunnel route dns 6bcb79c6-9893-4bac-aa61-7d858374ba4f rss.afeyoung.icu  # map dns to the tunnel
+ cloudflared tunnel route ip show
+ cloudflared tunnel run  # run the tunnel
+}
+
 cft() {
   # I don't have to login... Amazing..
   sudo $DIR/cloudflared tunnel -url $1
