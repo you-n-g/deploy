97a497efb9a00e6a83029e5886a22a871e0275c8
Young <afe.young@gmail.com>
2025-02-24 01:58:37 +0000

    feat: Add drop_params option and enhance dotenv script handling

--- helper_scripts/bin/mydotenv.sh
+++ helper_scripts/bin/mydotenv.sh
@@ -16,16 +16,26 @@ MY_ENV=${MY_ENV:-.env}
 # for example: `-f .another.env`
 # NOTE: It only use the last .env
 
-if [ -e $MY_ENV ]; then
-  echo "loading ${MY_ENV}..."
+run_single_env() {
+  local env_file=$1
+  echo "loading ${env_file}..."
   if command -v dotenv &> /dev/null; then
-    echo dotenv -f $MY_ENV $EXTRA_DOTENV_ARGS run -- "$@"
-    dotenv -f $MY_ENV $EXTRA_DOTENV_ARGS run -- "$@"
-    # `--override` is the default option; we don't have to add it
+    echo dotenv -f $env_file $EXTRA_DOTENV_ARGS run -- "${@:2}"
+    dotenv -f $env_file $EXTRA_DOTENV_ARGS run -- "${@:2}"
+    # `--override` is the default option we don't have to add it
   else
     echo "dotenv is not installed. Please install it (e.g. \`pip install python-dotenv\`) to use this script."
     exit 1
   fi
+}
+
+if [ -d $MY_ENV ]; then
+  # if it is a folder, then we run commands for each env.
+  for env_file in $MY_ENV/*.env; do
+    run_single_env $env_file "$@"
+  done
+elif [ -e $MY_ENV ]; then
+  run_single_env $MY_ENV "$@"
 else
   "$@"
 fi
