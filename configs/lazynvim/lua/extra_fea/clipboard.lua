dca74e45d148fa7c950d9f1cbc67d6c5ba804b88
Young <afe.young@gmail.com>
2025-11-17 07:51:31 +0000

    feat: enable tmux clipboard, update snippets, add myts script

--- configs/lazynvim/lua/extra_fea/clipboard.lua
+++ configs/lazynvim/lua/extra_fea/clipboard.lua
@@ -1,18 +1,18 @@
 -- NOTE: I found the default value is reasonable enough for me...
 -- - in WSL, it automatically uses the system clipboard
 -- - on server, it sync with tmux clipboard automatically
--- if vim.fn.system('tmux -V') ~= '' then
---   -- I prefer the tmux clipboard as "+*" (based on my experience, it is more safety than systemclipboard);
---   vim.g.clipboard = {
---     name = 'tmux',
---     copy = {
---       ['+'] = 'tmux load-buffer -w -',
---       ['*'] = 'tmux load-buffer -w -',
---     },
---     paste = {
---       ['+'] = 'tmux save-buffer -',
---       ['*'] = 'tmux save-buffer -',
---     },
---     cache_enabled = 1,
---   }
--- end
+if vim.fn.system('tmux -V') ~= '' then
+  -- I prefer the tmux clipboard as "+*" (based on my experience, it is more safety than systemclipboard);
+  vim.g.clipboard = {
+    name = 'tmux',
+    copy = {
+      ['+'] = 'tmux load-buffer -w -',
+      ['*'] = 'tmux load-buffer -w -',
+    },
+    paste = {
+      ['+'] = 'tmux save-buffer -',
+      ['*'] = 'tmux save-buffer -',
+    },
+    cache_enabled = 1,
+  }
+end
