60b4ecba8cf6f44448d1ba6f91fe2d8ae0e9fdef
you-n-g <you-n-g@users.noreply.github.com>
2026-03-10 22:18:50 +0800

    Update install_homebrew.sh

--- deploy_apps/install_homebrew.sh
+++ deploy_apps/install_homebrew.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # If Homebrew is already installed, skip download
 
+
+mkdir -p ~/bin/
+export PATH=$PATH:$HOME/bin/
+
 if [ ! -d ~/homebrew ]; then
   mkdir ~/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew
   ln -s ~/homebrew/bin/brew ~/bin/
