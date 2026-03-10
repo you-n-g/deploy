0f72f21c223c9f0ec8402fd22d7872cb3ee1d264
Xiao Yang <xiaoyang@Xiaos-MacBook-Pro.local>
2026-03-11 01:00:58 +0800

    basic mac env

--- MAC-based.sh
+++ MAC-based.sh
@@ -1,1 +1,26 @@
-brew install vim wget ctags-exuberant cmake
+#!/bin/bash
+
+deploy_apps/install_homebrew.sh
+xcode-select --install
+
+brew install --cask keyclu
+
+brew install fnm
+
+
+echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc  # mac has already use zsh as the default shell
+
+brew install uv
+
+brew install gnupg # it takes very long time
+brew install pinentry-mac
+mkdir -p ~/.gnupg
+echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+
+
+bash ./configs/llm/conf_llm.sh
+
+bash ./deploy_apps/install_zsh.sh
+
+# Optinal tools:
+brew install htop
--- deploy_apps/install_homebrew.sh
+++ deploy_apps/install_homebrew.sh
@@ -3,11 +3,14 @@
 
 
 mkdir -p ~/bin/
-export PATH=$PATH:$HOME/bin/
+export PATH=$HOME/bin/:$PATH
+
+echo 'export PATH=$HOME/bin/:$PATH' >> ~/.zshrc  # mac has already use zsh as the default shell
 
 if [ ! -d ~/homebrew ]; then
   mkdir ~/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew
   ln -s ~/homebrew/bin/brew ~/bin/
+  # homebrew will install executable link near the home brew
 fi
 
 ~/homebrew/bin/brew update
