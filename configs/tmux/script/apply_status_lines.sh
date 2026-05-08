680b51cee83f9489d2c318c351ea3d219ae1c90b
xiaoyang <afe.young@gmail.com>
2026-05-08 23:12:36 +0800

    Fix tmux status window formatting

--- configs/tmux/script/apply_status_lines.sh
+++ configs/tmux/script/apply_status_lines.sh
@@ -5,10 +5,17 @@ set -eu
 tmux set-option -g @status-window-second-line-reserve auto
 tmux set-option -g @status-non-window-fixed-right-width 58
 tmux set-option -g @status-buttons-expanded 0
+tmux set-option -g window-status-separator ''
 
 button_light='colour244'
 button_dark='colour242'
 button_fg='colour235'
+window_bg_even='colour214'
+window_fg_even='colour235'
+window_bg_odd='colour220'
+window_fg_odd='colour235'
+window_active_bg='colour196'
+window_active_fg='colour231'
 
 button() {
   range="$1"
@@ -37,6 +44,6 @@ expanded_buttons="$(
 tmux set-option -g @status-buttons-compact "$compact_buttons"
 tmux set-option -g @status-buttons-full "$expanded_buttons"
 left_buttons="#{?#{==:#{@status-buttons-expanded},1},#{@status-buttons-full},#{@status-buttons-compact}}"
-window_format='#[list=on align=left]#{W:#[range=window|#{window_index}]#[bg=#{?#{==:#{e|%:#{window_index},2},0},colour214,colour220}]#[fg=colour235]#I:#W#{window_flags}#[norange list=on default],#[range=window|#{window_index} list=focus]#[bg=colour196]#[fg=colour231]#[bold]#I:#W#{window_flags}#[norange list=on default]}'
+window_format="#[list=on align=left]#{W:#[range=window|#{window_index}]#[bg=#{?#{==:#{e|m:#{window_index},2},0},${window_bg_even},${window_bg_odd}}]#[fg=#{?#{==:#{e|m:#{window_index},2},0},${window_fg_even},${window_fg_odd}}]#[bold]#I#[nobold]#[underscore]#W#{window_flags}#[nounderscore]#[norange list=on default],#[range=window|#{window_index} list=focus]#[bg=${window_active_bg}]#[fg=${window_active_fg}]#[bold]#I#[nobold]#[underscore]#W#{window_flags}#[nounderscore]#[norange list=on default]}"
 
 tmux set-option -g status-format[1] "#[align=left]${left_buttons}#[default]${window_format}"
