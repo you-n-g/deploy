ff1709cb487564148354c01dbdf1b94bec065e8f
xiaoyang <afe.young@gmail.com>
2026-08-17 09:55:49 +0800

    Improve ranger tmux window detection

--- configs/tmux/script/ensure_ranger_fm.sh
+++ configs/tmux/script/ensure_ranger_fm.sh
@@ -16,9 +16,31 @@ find_ranger_window() {
   local pane_rows ps_rows
 
   pane_rows="$(tmux list-panes -s -t "$SESSION" -F $'#{window_id}\t#{pane_pid}' 2>/dev/null)"
-  ps_rows="$(ps -ax -o pid=,ppid=,comm= 2>/dev/null)"
+  ps_rows="$(ps -ax -o pid=,ppid=,comm=,args= 2>/dev/null)"
 
   awk -F '\t' '
+    function is_ranger_process(command, args, base) {
+      base = command
+      sub(/^.*\//, "", base)
+
+      # Direct binary/entrypoint launch.
+      if (base == "ranger") return 1
+      if (args ~ /^([^[:space:]]*\/)?ranger([[:space:]]|$)/) return 1
+      if (base ~ /^(zsh|bash|sh|fish)$/ && args ~ /(^|[[:space:]])-[^[:space:]]*c[[:space:]]+ranger([[:space:]]|$)/) return 1
+
+      # Current wrapper in configs/shell/rcfile.sh:
+      #   uvx --from ranger-fm python -c "from ranger.core.main import main; main()"
+      if (args ~ /(^|[[:space:]])uvx([[:space:]]|$)/ && args ~ /--from[[:space:]]+ranger-fm([[:space:]]|$)/) return 1
+      if (args ~ /(^|[[:space:]])uv([[:space:]]|$)/ && args ~ /--from[[:space:]]+ranger-fm([[:space:]]|$)/) return 1
+      if (args ~ /from[[:space:]]+ranger\.core\.main[[:space:]]+import[[:space:]]+main/ && args ~ /main\(\)/) return 1
+
+      # Other common Python forms.
+      if (args ~ /(^|[[:space:]])python[0-9.]*([[:space:]]|$)/ && args ~ /(^|[[:space:]])-m[[:space:]]+ranger([[:space:]]|$)/) return 1
+      if (args ~ /(^|[[:space:]])python[0-9.]*([[:space:]]|$)/ && args ~ /ranger\.core\.main/) return 1
+
+      return 0
+    }
+
     FNR == NR {
       if ($2 ~ /^[0-9]+$/) {
         root_window[$2] = $1
@@ -33,12 +55,13 @@ find_ranger_window() {
       if (proc[1] !~ /^[0-9]+$/) next
       parent[proc[1]] = proc[2]
       name[proc[1]] = proc[3]
+      args = line
+      sub(/^[0-9]+[[:space:]]+[0-9]+[[:space:]]+[^[:space:]]+[[:space:]]*/, "", args)
+      argv[proc[1]] = args
     }
     END {
       for (pid in name) {
-        command = name[pid]
-        sub(/^.*\//, "", command)
-        if (command != "ranger") continue
+        if (!is_ranger_process(name[pid], argv[pid])) continue
 
         current = pid
         while (current in parent) {
